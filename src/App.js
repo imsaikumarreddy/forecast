e1df5bcc431c7064332f12f5381564904593cf16
imsaikumarreddy <imsaikumarreddy@gmail.com>
2020-09-20 00:56:31 -0400

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,123 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WeatherIcon from './icons/weather.svg'
 
 function App() {
+  const RecentLocations = [
+    {
+      place: 'london'
+    },
+    {
+      place: 'usa'
+    },
+    {
+      place: 'aus'
+    }
+  ]
+
+  /*function Forecast(){
+    const UpcomingForecast = [
+      {
+        date: 10
+        day: "mon"
+
+             <p>day</p>
+             <img alt="weather icon" src={WeatherIcon} />
+              <p>temp</p>
+      }
+    ]
+  }*/
+  const LeftConatiner = () => {
+    return(
+      <div className="left-container">
+          <div className="add-location-container">
+            <input type="text" name="search" value="search" placeholder="Type city name" />
+            <button>send</button>
+          </div>
+          <div className="divider"></div>
+          <div className="recent-added-locations">
+            <p>Recent locations</p>
+            <div className="small-divider"></div>
+            <div className="added-locations">
+              {
+                RecentLocations.map((each,index)=>{
+                  return(
+                    <div className="recent-location" key={index}>
+                    <p>{each.place}</p>
+                  </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+          <div className="clear-container">
+            <button className="clear-button">Clear</button>
+          </div>
+        </div>
+    )
+  }
+
+const RightContainer = () => {
+  return(
+    <div className="right-container">
+          <div className="place-container">
+            <h2>Halifax</h2>
+            <i class="fa fa-refresh"></i>
+          </div>
+          <div className="weather-content">
+            <div className="content-icon">
+              <img alt="weather icon" src={WeatherIcon} />
+            </div>
+            <div className="content-detials">
+              <p>15c</p>
+              <p>clear sky</p>
+              <p>wind: 3s</p>
+              <p>pressure</p>
+            </div>
+          </div>
+          <div className="upcoming-forecast">
+            <div>
+              <p>date</p>
+             <p>day</p>
+             <img alt="weather icon" src={WeatherIcon} />
+              <p>temp</p>
+            </div>
+            <div>
+              <p>date</p>
+             <p>day</p>
+             <img alt="weather icon" src={WeatherIcon} />
+              <p>temp</p>
+            </div>
+            <div>
+              <p>date</p>
+             <p>day</p>
+             <img alt="weather icon" src={WeatherIcon} />
+              <p>temp</p>
+            </div>
+            <div>
+              <p>date</p>
+             <p>day</p>
+             <img alt="weather icon" src={WeatherIcon} />
+              <p>temp</p>
+            </div>
+            <div>
+              <p>date</p>
+             <p>day</p>
+             <img alt="weather icon" src={WeatherIcon} />
+              <p>temp</p>
+            </div>
+            
+          </div>
+   </div>
+  )
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-container">
+       <LeftConatiner />
+       <RightContainer/>
+      </div>
     </div>
   );
 }
