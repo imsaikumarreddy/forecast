5f67331b92a64738cb54d3123a931af04e85784c
imsaikumarreddy <imsaikumarreddy@gmail.com>
2020-09-22 15:24:30 -0400

    css completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,82 @@
 import React,{useState} from 'react';
 import './App.css';
 import LeftConatiner from './LeftContainer'
-import WeatherIcon from './icons/weather.svg'
+import Rain from './icons/amcharts_weather_icons_1.0.0/animated/rainy-7.svg'
+import clouds from './icons/amcharts_weather_icons_1.0.0/animated/cloudy-day-1.svg'
+import clear from './icons/amcharts_weather_icons_1.0.0/animated/day.svg'
+import Haze from './icons/amcharts_weather_icons_1.0.0/animated/snowy-5.svg'
+import mist from './icons/amcharts_weather_icons_1.0.0/animated/mist.svg'
+import axios from 'axios'
 
 function App() {
   const [city,SetCity] = useState('')
+  const [OpenWeatherApiKey] = useState('c51223c219d6aec8cb8c5210449bd859')
+  const [RecentlyAddedLocations,SetRecentlyAddedLocations] = useState([])
   const [showMore,SetShowMore] = useState({})
+
+  const GetCityWeather = async() => {
+    let tempdata = RecentlyAddedLocations
+    let { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OpenWeatherApiKey}`)
+    let Forecast = await  axios.get(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=5&appid=${OpenWeatherApiKey}`)
+    data.Forecast = Forecast.data.list
+    if (RecentlyAddedLocations.length > 7) {
+        tempdata.splice(0,1)
+    }
+    SetRecentlyAddedLocations(tempdata.concat(data))
+    SetCity('')
+    ShowMoreDetails(data)
+  }
+   const handleClick = () => {
+        GetCityWeather()
+    }
+   const clearRecentAdded = () =>{
+        SetRecentlyAddedLocations([])
+        SetShowMore({})
+    }
+
   const ShowMoreDetails = (data) => {
     SetShowMore(data)
   }
+  const toCelsius = (f) => {
+    return (f-273.15);
+  }
+
+  const customDate = (value) => {
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let date= new Date(value*1000);
+    return date.getDate()+" "+days[date.getDay()];
+  }
+
+  const RenderIcon = (data) => {
+    switch (data) {
+      case "Haze":
+        return Haze
+
+      case "Clouds":
+        return clouds
+
+      case "Clear":
+        return clear
+
+      case "Rain":
+        return Rain
+
+      case "Mist":
+        return mist;
+      default:
+        break;
+    }
+  }
+
+  const reload = async(each) =>{
+    let mydata = RecentlyAddedLocations
+    let newIndex = mydata.findIndex(r=>r.id === each.id)
+    let { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${each.name}&appid=${OpenWeatherApiKey}`)
+    let Forecast = await  axios.get(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${each.name}&cnt=5&appid=${OpenWeatherApiKey}`)
+    data.Forecast = Forecast.data.list
+    mydata.splice(newIndex, 1, data);
+    SetRecentlyAddedLocations(mydata)
+}
   const RightContainer = () => {
     return(
       <div className="right-container">
@@ -17,65 +85,62 @@ function App() {
           (<div>
             <div className="place-container">
               <h2 className="header-text">{showMore.name}</h2>
-              <i class="fa fa-refresh"></i>
+              <i onClick={()=> reload(showMore)} className="fa fa-refresh"></i>
             </div>
             <div className="weather-content">
               <div className="content-icon">
-                <img alt="weather icon" src={WeatherIcon} />
+                <img alt="weather icon" src={RenderIcon(showMore.weather[0].main)}/>
               </div>
               <div className="content-detials">
-                <p>{showMore.main.temp} F</p>
+                <p>{Math.round(toCelsius(showMore.main.temp))} C</p>
                 <p>{showMore.weather[0].description}</p>
-                <p>{showMore.wind.speed}s</p>
-                <p>{showMore.main.pressure}</p>
+                <p>Wind: {showMore.wind.speed}ms {showMore.wind.deg} deg </p>
+                <p>Pressure: {showMore.main.pressure}</p>
               </div>
             </div>
-            <div className="upcoming-forecast">
-              <div>
-                <p>date</p>
-              <p>day</p>
-              <img alt="weather icon" src={WeatherIcon} />
-                <p>temp</p>
+              <div className="upcoming-forecast">
+                
+                {
+                    showMore.Forecast.map((each,index)=>{
+                      return(
+                        <div key={index}>
+                          <p>{customDate(each.dt)}</p>
+                          <img alt="weather icon" src={RenderIcon(each.weather[0].main)} />
+                          <p>{Math.round(toCelsius(each.temp.day))} C</p>
+                        </div>
+                      )
+                    })
+                }
               </div>
-              <div>
-                <p>date</p>
-              <p>day</p>
-              <img alt="weather icon" src={WeatherIcon} />
-                <p>temp</p>
-              </div>
-              <div>
-                <p>date</p>
-              <p>day</p>
-              <img alt="weather icon" src={WeatherIcon} />
-                <p>temp</p>
-              </div>
-              <div>
-                <p>date</p>
-              <p>day</p>
-              <img alt="weather icon" src={WeatherIcon} />
-                <p>temp</p>
-              </div>
-              <div>
-                <p>date</p>
-              <p>day</p>
-              <img alt="weather icon" src={WeatherIcon} />
-                <p>temp</p>
-              </div>
-              
             </div>
-          </div>)
+          )
           : null
         }
     </div>
     )
   }
 
-
+  const clearLocation= (each) => {
+    let newIndex = RecentlyAddedLocations.findIndex(r=>r.id === each.id)
+    if (newIndex !== -1) {
+      RecentlyAddedLocations.splice(newIndex,1)
+      SetRecentlyAddedLocations(RecentlyAddedLocations)
+    }
+}
 
   return (
     <div className="App">
       <div className="app-container">
-       <LeftConatiner ShowMoreDetails={ShowMoreDetails} />
+       <LeftConatiner 
+       handleClick={handleClick}
+       OpenWeatherApiKey={OpenWeatherApiKey}
+       clearRecentAdded={clearRecentAdded}
+       RecentlyAddedLocations={RecentlyAddedLocations}
+       SetCity={SetCity}
+       clearLocation={clearLocation}
+       city={city} ShowMoreDetails={ShowMoreDetails} 
+       toCelsius={toCelsius} reload={reload}
+       RenderIcon={RenderIcon} />
        <RightContainer />
       </div>
     </div>
--- src/LeftContainer.js
+++ src/LeftContainer.js
@@ -1,37 +1,13 @@
 import React from 'react'
-import axios from 'axios'
 
-export default class LeftContainer extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            city: '',
-            RecentlyAddedLocations: []
-        }
-    }
-    GetCityWeather = async() => {
-        let { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=6315533839f238d2210a6f0919e199b8`)
-        let Forecast = await  axios.get(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${this.state.city}&cnt=5&appid=6315533839f238d2210a6f0919e199b8`)
-        console.log(Forecast);
-        this.setState({RecentlyAddedLocations: this.state.RecentlyAddedLocations.concat(data),city:''})
-        this.props.ShowMoreDetails(data)
-    }
-    handleClick = () => {
-        this.GetCityWeather()
-    }
-    clearRecentAdded(){
-        this.setState({RecentlyAddedLocations: []})
-    }
-    clearLocation(){
-        this.setState({})
-    }
-
-    render(){
+export default function LeftContainer (props){
+   
+        let { toCelsius,RenderIcon,clearRecentAdded, clearLocation, SetCity ,ShowMoreDetails, city, RecentlyAddedLocations, handleClick, reload }= props;
         return(
             <div className="left-container">
                 <div className="add-location-container">
-                    <input type="text" name="city" value={this.state.city} placeholder="Type city name" onChange={(e)=>this.setState({city:e.target.value})} />
-                    <i class="fa fa-plus-circle" onClick={this.handleClick}></i>
+                    <input type="text" name="city" value={city} placeholder="Type city name" onChange={(e)=>SetCity(e.target.value)} />
+                    <i className="fa fa-plus-circle" onClick={handleClick}></i>
                 </div>
                 <div className="divider"></div>
                 <div className="recent-added-locations">
@@ -39,22 +15,28 @@ export default class LeftContainer extends React.Component{
                     <div className="small-divider"></div>
                     <div className="added-locations">
                     {
-                        this.state.RecentlyAddedLocations.map((each,index)=>{
+                        RecentlyAddedLocations.map((each,index)=>{
                         return(
                             <div className="recent-location" key={index}>
-                            <p onClick={()=>this.props.ShowMoreDetails(each)}>{each.name}</p>
-                            <i class="fa fa-refresh" onClick={()=>this.reload()}></i>
-                            <i class="fa fa-close" onClick={()=>this.clearLocation()}></i>
-                        </div>
+                                <p className="cursor" onClick={()=>ShowMoreDetails(each)}>{each.name}</p>
+                                <div className="temp-container">
+                                    <p>{Math.round(toCelsius(each.main.temp))}C</p>
+                                    <img alt="weather icon" src={RenderIcon(each.weather[0].main)} />
+                                </div>
+                                <p>{each.weather[0].main}</p>
+                                <div className="actions-container">
+                                    <i className="fa fa-refresh" onClick={()=>reload(each)}></i>
+                                    <i className="fa fa-close" onClick={()=>clearLocation(each)}></i>
+                                </div>
+                            </div>
                         )
                         })
                     }
                     </div>
                 </div>
                 <div className="clear-container">
-                    <button className="clear-button" onClick={()=>this.clearRecentAdded()}>Clear</button>
+                    <button className="clear-button" onClick={()=>clearRecentAdded()}>Clear</button>
                 </div>
             </div>
         )
-    }
 }
